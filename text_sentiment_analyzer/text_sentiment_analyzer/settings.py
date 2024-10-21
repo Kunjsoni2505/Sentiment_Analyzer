e7b2456934fa753b91581df9da0b1c8abc0a3599
kunj <kunjansoni169@gmail.com>
2024-10-21 22:23:04 +0530

    final commit

--- text_sentiment_analyzer/text_sentiment_analyzer/settings.py
+++ text_sentiment_analyzer/text_sentiment_analyzer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a7w1b^$$z@n%iic)$8#+_u5u8c(l59(40qa!mm(5t)2p3ajget
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
 
 
 # Application definition
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
