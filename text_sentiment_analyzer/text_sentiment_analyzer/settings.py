478ca82ae70d60e0c62903ebd6db9bf82f1eb2d0
kunj <kunjansoni169@gmail.com>
2025-02-21 22:52:12 +0530

    latest commit

--- text_sentiment_analyzer/text_sentiment_analyzer/settings.py
+++ text_sentiment_analyzer/text_sentiment_analyzer/settings.py
@@ -9,7 +9,7 @@ MODEL_PATH = os.path.join(BASE_DIR, 'sentiment_analyzer', 'rnn_model.h5')  # Upd
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-a7w1b^$$z@n%iic)$8#+_u5u8c(l59(40qa!mm(5t)2p3ajget')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.railway.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 
 
